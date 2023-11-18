ac71083aa3d890fafc9287d92adb7ffca979f8ac
Martin Trautwein <48868501+liquid-matra@users.noreply.github.com>
2023-11-18 23:22:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,19 @@ APP_INSTANCES=$5
 case ${PLACEMENT_ZONE} in
 
   IPZ001)
-    STATEMENTS
+    url="https://cf.ipz001.api.io"
     ;;
 
   EPZ001)
-    STATEMENTS
+    url="https://cf.epz001.api.io"
     ;;
 
   IPZ002)
-    STATEMENTS
+    url="https://cf.ipz002.api.io"
     ;;
 
   EPZ002)
-    STATEMENTS
+    url="https://cf.epz002.api.io"
     ;;
 
   *)
@@ -32,7 +32,7 @@ esac
 
 echo "login to BPC Cloud Foundry"
 echo "placement-zone: ${PLACEMENT_ZONE} organization: ${ORGANIZATION_NAME} space: ${SPACE_NAME}"
-
+echo "cf login -a $url -u username -p ***** -o ${ORGANIZATION_NAME} -s ${SPACE_NAME}"
 echo "cf push ${APP_NAME} -i ${APP_INSTANCES} "
 
 time=$(date)
